c7f7e3021c8da3462e34a9bd05a3b5cd311b92cc
jsaispass777 <rvy2yb5d4j@privaterelay.appleid.com>
2026-07-30 05:21:21 +0200

    Create Créermolécule.js

---
+++ Créermolécule.js
@@ -0,0 +1,5 @@
+export const library = {
+  "ADME / CYP450": ["paracetamol", "warfarin", "omeprazole"],
+  "Substances psychoactives": ["caffeine", "morphine", "cocaine"],
+  "Toxicité aiguë classique": ["aspirin", "digoxin", "methanol"],
+};
